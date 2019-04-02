d342ddf567569adbe6cded46a552cafc3a9307cf
Pius Friesch <p.friesch@posteo.de>
2019-04-03 01:27:32 +0200

    update (no commit msg)

--- base/base_model.py
+++ base/base_model.py
@@ -1,7 +1,9 @@
+import torch
 import torch.nn as nn
 import numpy as np
 
 from utils.logger_config import logger
+from thop import profile
 
 # @see https://stackoverflow.com/questions/3154460/python-human-readable-large-numbers/3155023
 import math
@@ -43,7 +45,18 @@ class BaseModel(nn.Module):
         Model summary
         """
 
-        logger.info('Trainable parameters: {}'.format(self.trainable_parameters()))
+        if self.batch_ordering == "NCL":
+            flops, params = profile(self, input={'fbank': torch.zeros(((8, 40, sum(self.context) + 50)))},
+                                    custom_ops=self.get_custom_ops_for_counting())
+        elif self.batch_ordering == "TNCL":
+            flops, params = profile(self, input={'fbank': torch.zeros(((1, 8, 40, 1)))})
+        else:
+            raise NotImplementedError
+
+        logger.info(
+            'Trainable parameters: {}'.format(self.trainable_parameters())
+            + f'\nFLOPS: ~{millify(flops)} ({flops})\n')
+
         logger.info(self)
 
     def info(self):
